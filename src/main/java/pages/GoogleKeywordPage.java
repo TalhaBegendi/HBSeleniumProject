0341bc3f5b7e9004a1931a9d904d404f9b5b7907
TalhaBegendi <Talhabegendi3@gmail.com>
2022-09-05 21:19:43 +0300

    HBSeleniumProject
    HBSeleniumProject

--- src/main/java/pages/GoogleKeywordPage.java
+++ src/main/java/pages/GoogleKeywordPage.java
@@ -1,15 +1,24 @@
 package pages;
+
+import com.google.gson.JsonObject;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.Component.SearchResults;
 import utils.PropertyManager;
 import utils.Utilities;
 import org.apache.log4j.Logger;
+
+import java.util.List;
+
+
 public class GoogleKeywordPage extends Utilities  {
     final static Logger logger = Logger.getLogger(GoogleKeywordPage.class);
     @FindBy(css = "[name='q']")
     public WebElement googleKeywordInput;
     @FindBy(css = "[name='btnK']")
     public WebElement googleSearchButton;
+    @FindBy(css = "[data-sokoban-container]")
+    public List<SearchResults> results;
     public void googleKeywordSearch() {
         PropertyManager propertyManager = new PropertyManager();
         String keyword = propertyManager.getProperty("env.properties", "keyword");
@@ -17,5 +26,12 @@ public class GoogleKeywordPage extends Utilities  {
         logger.info("Keyword entered for Google");
         elementClick(googleSearchButton);
         logger.info("Search for Google button clicked.");
+        JsonObject json = new JsonObject();
+        for (SearchResults result:results)
+        {
+            json.addProperty("title",result.getTitle());
+            json.addProperty("detail",result.getDetail());
+            json.addProperty("link",result.getLink());
+        }
     }
 }
--- src/main/java/pages/YandexKeywordPage.java
+++ src/main/java/pages/YandexKeywordPage.java
@@ -1,15 +1,23 @@
 package pages;
+import com.google.gson.JsonObject;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.Component.SearchResults;
 import utils.PropertyManager;
 import utils.Utilities;
+
+import javax.naming.directory.SearchResult;
+import java.util.List;
+
 public class YandexKeywordPage extends Utilities {
     final static Logger logger = Logger.getLogger(YandexKeywordPage.class);
     @FindBy(css = "[id='text']")
     public WebElement yandexKeywordInput;
     @FindBy(css = "[role='button']")
     public WebElement yandexSearchButton;
+    @FindBy(css = "[data-sokoban-container]")
+    public List<SearchResults> results;
     public void yandexKeywordSearch() {
         PropertyManager propertyManager = new PropertyManager();
         String keyword = propertyManager.getProperty("env.properties", "keyword");
@@ -17,6 +25,13 @@ public class YandexKeywordPage extends Utilities {
         logger.info("Keyword entered for Yandex");
         elementClick(yandexSearchButton);
         logger.info("Search for Yandex button clicked.");
+        JsonObject json = new JsonObject();
+        for (SearchResults result:results)
+        {
+            json.addProperty("title",result.getTitle());
+            json.addProperty("detail",result.getDetail());
+            json.addProperty("link",result.getLink());
+        }
     }
 }
 
